84f1d4d8549fab1f7935dbf8f2835ddcff37b34e
Abarna-75 <appunsw@gmail.com>
2021-11-10 16:24:18 +1100

    cart pagetest newly added

---
+++ src/test/java/com/qa/opencart/Tests/CartTest.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest {
+
+	@Test
+	public void testCart()
+	{
+		
+		Assert.assertEquals(true, true);
+	}
+}
