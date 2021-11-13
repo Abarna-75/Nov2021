984e6923e6a0f9137710dc08d57e980e417cc457
Abarna-75 <appunsw@gmail.com>
2021-11-13 14:38:39 +1100

    Saturday changes 1

---
+++ src/test/java/com/qa/opencart/Tests/Saturday.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.Tests;
+
+public class Saturday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("saturday change 1 ");
+		
+		
+	}
+
+}
